628258a3365b9fe22a99beb54e1af2d2e6b06693
mattfro333 <mattfro333@gmail.com>
2017-04-26 18:57:53 -0600

    Background

--- server.js
+++ server.js
@@ -14,6 +14,13 @@ const app = express();
 // Connect to front end!!!!
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+
 app.use(session({
   resave: true,
   saveUninitialized: true,
