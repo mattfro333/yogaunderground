f164835925a205a8ed0f0fcaf1c4385246aec44a
mattfro333 <mattfro333@gmail.com>
2019-05-11 20:58:10 -0600

    reworking the clone probably have to start from the ground up

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express =  require('express'),
       session = require('express-session'),
       bodyParser = require('body-parser'),
-      massive = require('massive'),
+      // massive = require('massive'),
       passport =  require('passport'),
       Auth0Strategy = require('passport-auth0'),
       config = require('./config'),
@@ -31,9 +31,9 @@ app.use(passport.session());
 
 app.use(express.static(__dirname));
 
-const massiveInstance = massive.connectSync({connectionString: 'postgres://postgres:Milkyway6933@localhost/yoga'})
+// const massiveInstance = massive.connectSync({connectionString: 'postgres://postgres:Milkyway6933@localhost/yoga'})
 
-app.set('db', massiveInstance);
+app.set('db');
 const db = app.get('db');
 
 // db.createUser(function(err, user) {
@@ -169,85 +169,85 @@ app.post('/api/payment', function(req, res, next){
 //     })
 // }
 // ));
-db.init.createPaymentTable([], function(err, results){
-  if (err){
-    console.error(err);
-  } else {
-    console.log("Initialized Payment Table");
-  }
-})
-module.exports = massiveInstance;
-var paymentCtrl = {
-create:function(req, res, next){
-  db.payment.create_payment([
-    req.body.name,
-    req.body.amount,
-    req.body.date,
-    req.body.userid
-  ], function(err, results){
-    if (err){
-      console.error(err);
-      return res.send(err);
-    }else {
-      res.send(results)
-    }
-  })
-},
- getPayments:function(req, res){
-   db.payment.read_payments([], function(err, results){
-     if(err){
-       console.error(err);
-       return res.send(err);
-     }
-     return res.send(results);
-   })
- },
-   getPayment:function(req, res){
-     db.payment.read_payment([req.params.paymentId],
-     function(err, results){
-       if (err){
-         console.error(err);
-         return res.send(err);
-       }
-       if (results.length == 0){
-         return res.status(404).send("No payment found.")
-       }
-       return res.send(results[0]);
-     })
-   },
-   update:function(req, res){
-     db.payment.update_payment([
-       req.body.name,
-       req.body.amount,
-       req.body.date,
-       req.body.userid
-     ],
-   function(err, results){
-     if(err){
-       console.error(err);
-       return res.send(err);
-     }
-     return res.send(results[0]);
-   })
- },
-   delete:function(req, res){
-     db.payment.delete_payment([req.params.paymentId], function(err, results){
-       if (err){
-         console.error(err);
-         return res.send(err);
-       }
-       if (results.length === 0){
-         return res.status(404).send("Payment not found.");
-       }
-       res.send('Payment for ' + results[0].name + ' has been deleted.');
-     })
-   }
- }
-app.get('/api/payments', paymentCtrl.getPayments);
-app.get('/api/payment/:paymentId', paymentCtrl.getPayment);
-app.post('/api/infopayment', paymentCtrl.create);
-app.put('/api/payment/:paymentId', paymentCtrl.update);
-app.delete('/api/payment/:paymentId', paymentCtrl.delete)
+// db.init.createPaymentTable([], function(err, results){
+//   if (err){
+//     console.error(err);
+//   } else {
+//     console.log("Initialized Payment Table");
+//   }
+// })
+// module.exports = massiveInstance;
+// var paymentCtrl = {
+// create:function(req, res, next){
+//   db.payment.create_payment([
+//     req.body.name,
+//     req.body.amount,
+//     req.body.date,
+//     req.body.userid
+//   ], function(err, results){
+//     if (err){
+//       console.error(err);
+//       return res.send(err);
+//     }else {
+//       res.send(results)
+//     }
+//   })
+// },
+//  getPayments:function(req, res){
+//    db.payment.read_payments([], function(err, results){
+//      if(err){
+//        console.error(err);
+//        return res.send(err);
+//      }
+//      return res.send(results);
+//    })
+//  },
+//    getPayment:function(req, res){
+//      db.payment.read_payment([req.params.paymentId],
+//      function(err, results){
+//        if (err){
+//          console.error(err);
+//          return res.send(err);
+//        }
+//        if (results.length == 0){
+//          return res.status(404).send("No payment found.")
+//        }
+//        return res.send(results[0]);
+//      })
+//    },
+//    update:function(req, res){
+//      db.payment.update_payment([
+//        req.body.name,
+//        req.body.amount,
+//        req.body.date,
+//        req.body.userid
+//      ],
+//    function(err, results){
+//      if(err){
+//        console.error(err);
+//        return res.send(err);
+//      }
+//      return res.send(results[0]);
+//    })
+//  },
+//    delete:function(req, res){
+//      db.payment.delete_payment([req.params.paymentId], function(err, results){
+//        if (err){
+//          console.error(err);
+//          return res.send(err);
+//        }
+//        if (results.length === 0){
+//          return res.status(404).send("Payment not found.");
+//        }
+//        res.send('Payment for ' + results[0].name + ' has been deleted.');
+//      })
+//    }
+//  }
+// app.get('/api/payments', paymentCtrl.getPayments);
+// app.get('/api/payment/:paymentId', paymentCtrl.getPayment);
+// app.post('/api/infopayment', paymentCtrl.create);
+// app.put('/api/payment/:paymentId', paymentCtrl.update);
+// app.delete('/api/payment/:paymentId', paymentCtrl.delete)
 
 app.listen(3000, function() {
   console.log('Connected on 3000')
